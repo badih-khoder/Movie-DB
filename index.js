40ca0231a056b8582d43cab793d2370f17786951
nazim nasser <badih123khoder@gmail.com>
2021-12-31 21:03:49 +0200

    step 5

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
+
+
+
 var time = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();
 
 app.get('/', (req, res) => {
@@ -31,6 +41,43 @@ app.get('/hello/', (req, res) => {
 app.get('/search', (req, res) => {
     if (req.query.s) { res.json({ status: 200, message: "Ok", data: req.query.s }) } else { res.status(500).json({ status: 500, error: true, message: "you have to provide a search" }) }
 })
+
+
+
+
+
+
+
+
+
+
+
+app.get('/movies/create', (req, res) => {
+
+})
+
+app.get('/movies/read', (req, res) => {
+    res.json({ "status": 200, "message": movies })
+
+})
+
+app.get('/movies/update', (req, res) => {
+
+})
+
+app.get('/movies/delete', (req, res) => {
+
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
