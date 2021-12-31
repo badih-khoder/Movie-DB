c25927a2edbb02580959bc496d41e8a78cc71cb2
nazim nasser <badih123khoder@gmail.com>
2021-12-31 19:13:18 +0200

    step 3

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var time = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();
+
 app.get('/', (req, res) => {
     res.send('OK')
 })
+
+app.get('/test', (req, res) => {
+    res.json({ "status": 200, "message": "ok" })
+})
+
+
+app.get('/time', (req, res) => {
+    res.json({ "status": 200, "message": time })
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
