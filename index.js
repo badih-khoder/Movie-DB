1530d9877aa31684ad2350924fa3337afa0b61f9
nazim nasser <badih123khoder@gmail.com>
2022-01-12 11:27:26 +0200

    step 13

--- index.js
+++ index.js
@@ -4,23 +4,111 @@ const port = 3000
 
 
 const mongoose = require('mongoose')
-mongoose.connect(
-        "mongodb+srv://user:user@cluster0.ztjie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://user:user@cluster0.arxci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     .then(() => console.log("database connected")).catch(error => console.log(error))
-
-
 
 
 
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-//const movies = [
-//    { title: 'Jaws', year: 1975, rating: 8 },
-//    { title: 'Avatar', year: 2009, rating: 7.8 },
-//    { title: 'Brazil', year: 1985, rating: 8 },
-//    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
-//]
+
+
+
+const users = [
+    { username: 'ouss', password: 'hummus' },
+    { username: 'badih', password: 'shawerma' },
+]
+
+
+app.get('users/search', (req, res) => {
+    const search = req.query.s;
+    if (typeof search != 'undefined') {
+        const response = {
+            status: 200,
+            message: "ok",
+            data: search
+        };
+        res.send(response);
+    } else {
+        const response = {
+            status: 500,
+            error: true,
+            message: "you have to provide a search"
+        };
+        res.status(500);
+        res.send(response);
+    }
+});
+
+
+app.get('/users/read', (req, res) => {
+    res.json({ "status": 200, "message": users })
+})
+pp.get('/users/read/by-username', (req, res) => {
+    res.json({
+        "status": 200,
+        "data": users.sort((a, b) => {
+            if (a.username.toLowerCase() < b.username.toLowerCase())
+                return -1
+            if ((a.username.toLowerCase() > b.username.toLowerCase()))
+                return 1
+            return 0
+        })
+    })
+})
+
+app.get('/users/read/id/:id', (req, res) => {
+    let id = req.params.id
+    if (id) {
+        if (id < users.length) {
+            res.json({ status: 200, data: users[id] })
+        } else if (id == null || id === undefined) {
+            res.json({ status: 404, error: "You didnt add an id!" })
+        } else {
+            res.status(404).json({ status: 404, error: true, message: "the uder with " + id + " does not exist!" })
+        }
+    }
+})
+
+
+
+app.post('/users/create', (req, res) => {
+    if (!req.query.username || !req.query.password) {
+        res.json({ status: 403, error: true, message: 'You cant create an account without a username or without a password' })
+    } else {
+        let username = req.query.username;
+        let password = req.query.password;
+        users.push({ username, password });
+        res.json({ status: 200, data: users });
+    }
+})
+
+
+app.delete('/users/delete/:ID', (req, res) => {
+    let deleteduser = req.params.ID
+    if (users.length < deleteduser) {
+        res.json({ status: 404, error: true, message: 'the user with the ' + ID + ' does not exist' })
+    } else {
+        users.splice(deleteduser, 1)
+        res.json({ status: 200, data: users })
+    }
+})
+
+
+
+app.put('/users/update/:ID', (req, res) => {
+    var usernamex = req.query.username;
+    var passwordx = req.query.password;
+    if (!req.query.username) {
+        var usernamex = users[req.params.ID].username
+    }
+    if (!req.query.password) {
+        var passwordx = users[req.params.ID].password
+    }
+    users[req.params.ID] = { username: usernamex, password: passwordx };
+    res.json({ status: 200, data: users })
+})
 
 
 const movieshcema = new mongoose.Schema({
@@ -29,17 +117,15 @@ const movieshcema = new mongoose.Schema({
     rating: { type: Number, default: 4 },
 }, { versionKey: false });
 const movies = mongoose.model("movies", movieshcema);
-//movies.create({ title: 'Jaws', year: 1975, rating: 8 }, { title: 'Avatar', year: 2009, rating: 7.8 }, { title: 'Brazil', year: 1985, rating: 8 }, { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 })
 
 
 
 
-//Step-2
 app.get('/', (req, res) => {
     res.send('ok')
 })
 
-//Step-3
+
 app.get('/test', (req, res) => {
     res.json({ "status": 200, "message": "ok" })
 })
@@ -75,10 +161,6 @@ app.get('/search', (req, res) => {
     }
 });
 
-//Step-5
-//app.get('/movies/read', (req, res) => {
-//    res.json({ "status": 200, "message": movies })
-//})
 
 app.get("/movies/read", (req, res) => {
     movies
@@ -87,11 +169,6 @@ app.get("/movies/read", (req, res) => {
         .catch((error) => console.log("error no data found"));
 });
 
-
-//Step-6
-//app.get('/movies/read/by-date',(req,res)=>{
-//    res.json({"status":200,"data":movies.sort(function(a, b){return a.year - b.year})})
-//})
 
 app.get("/movies/read/by-date", (req, res) => {
     movies
@@ -110,10 +187,6 @@ app.get("/movies/read/by-date", (req, res) => {
 
 
 
-//app.get('/movies/read/by-rating',(req,res)=>{
-//    res.json({"status":200,"data":movies.sort(function(a, b){return b.rating - a.rating})})
-//})
-
 app.get("/movies/read/by-rating", (req, res) => {
     movies
         .find()
@@ -128,18 +201,6 @@ app.get("/movies/read/by-rating", (req, res) => {
         .catch((error) => console.log("error no data found"));
 });
 
-
-//app.get('/movies/read/by-title', (req, res) => {
-//    res.json({
-//        "status": 200, "data": movies.sort((a, b) => {
-//            if (a.title.toLowerCase() < b.title.toLowerCase())
-//                return -1
-//            if ((a.title.toLowerCase() > b.title.toLowerCase()))
-//                return 1
-//            return 0
-//        })
-//    })
-//})
 
 app.get("/movies/read/by-title", (req, res) => {
     movies
@@ -157,22 +218,6 @@ app.get("/movies/read/by-title", (req, res) => {
 
 
 
-//Step-7
-//app.get('/movies/read/id/:id', (req, res) => {
-//    let id = req.params.id
-//    if (id) {
-//        if (id < movies.length) {
-//            res.json({status:200,data:movies[id]})
-//        }
-//        else if(id == null || id=== undefined) {
-//            res.json({status:404,error:"You didnt add an id!"})
-//        }
-//        else{
-//            res.status(404).json({status:404,error:true, message:"the movie " +id+ " does not exist"} )
-//        }
-//    }
-//})
-
 app.get('/movies/read/id/:id', (req, res) => {
     let id = req.params.id
     movies.findById(id).then(data => {
@@ -183,10 +228,6 @@ app.get('/movies/read/id/:id', (req, res) => {
         })
 });
 
-
-
-
-//step-8
 app.post('/movies/create', (req, res) => {
     if (!req.query.rating) {
         req.query.rating = 4;
@@ -211,18 +252,6 @@ app.post('/movies/create', (req, res) => {
 
 
 
-//step-9
-//app.delete('/movies/delete/:ID', (req, res)=> {
-//let deletedmovie=req.params.ID
-//if (movies.length<deletedmovie){
-//res.json({status:404, error:true, message:'the movie <ID> does not exist'})
-//}
-//else{
-//movies.splice(deletedmovie, 1)
-//res.json ({status:200, data:movies})}
-//})
-
-
 app.delete('/movies/delete/:ID', (req, res) => {
     let deletedmovie = req.params.ID
     movies.findByIdAndDelete(req.params.ID).then(deletedmovie => {
@@ -237,25 +266,6 @@ app.delete('/movies/delete/:ID', (req, res) => {
         })
 })
 
-
-//Step-10
-//app.put('/movies/update/:ID', (req, res) => {
-//    var titlex = req.query.title;
-//    var yearx = req.query.year;
-//    var ratingx = req.query.rating;
-//if(!req.query.title){
-//    var titlex=movies[req.params.ID].title
-//}
-//if(!req.query.rating){
-//    var ratingx=movies[req.params.ID].rating
-//}
-//if(!req.query.year){
-//    var yearx=movies[req.params.ID].year
-//}
-//    movies[req.params.ID] = { title: titlex, year: yearx, rating: ratingx };
-//    res.json({ status: 200, data: movies })
-//
-//})
 
 
 
